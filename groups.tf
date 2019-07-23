1f58f8c5571b9d95a2d99e9f5518accb9affbbf3
Foivos Filippopoulos <ffilippopoulos@utilitywarehouse.co.uk>
2019-07-23 09:37:44 +0100

    Add missing storage-nodes group

--- groups.tf
+++ groups.tf
@@ -53,4 +53,17 @@ resource "matchbox_group" "worker" {
   }
 }
 
+resource "matchbox_group" "storage-node" {
+  count   = var.storage_node_count
+  name    = "storage-node-${count.index}"
+  profile = matchbox_profile.storage-node[count.index].name
+
+  selector = {
+    mac = null_resource.storage-nodes.*.triggers.mac_address[count.index]
+  }
+
+  metadata = {
+    ignition_endpoint = "${var.matchbox_http_endpoint}/ignition"
+  }
+}
 
