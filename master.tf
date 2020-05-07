9e6e30625ce46fc410c60664ae5939ffbc657403
Foivos Filippopoulos <ffilippopoulos@utilitywarehouse.co.uk>
2020-05-07 17:10:24 +0100

    Allow cluster service ips to proxy via masters
    in case externalTrafficPolicy: Cluster (default) is used for services

--- master.tf
+++ master.tf
@@ -64,6 +64,8 @@ data "ignition_file" "master_iptables_rules" {
 -A INPUT -p tcp -m tcp -s "${var.ssh_address_range}" --dport 22 -m conntrack --ctstate NEW,ESTABLISHED -j ACCEPT
 # Allow pod subnet input https://github.com/kubernetes/kubeadm/issues/1461#issuecomment-489362994
 -A INPUT -s "${var.pod_network}" -j ACCEPT
+# Allow packets destined to internal services ip range
+-A INPUT -d "${var.cluster_internal_svc_subnet}" -j ACCEPT
 # Allow masters to talk
 -A INPUT -p tcp -m tcp -s "${var.masters_subnet_cidr}" -j ACCEPT
 -A INPUT -p udp -m udp -s "${var.masters_subnet_cidr}" -j ACCEPT
