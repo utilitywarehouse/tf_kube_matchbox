bd7e1c6a386c16746faed4b5e15822f48f31545e
Foivos Filippopoulos <ffilippopoulos@uw.co.uk>
2023-07-27 10:51:54 +0100

    Output local cfssl volume id

--- output.tf
+++ output.tf
@@ -1,3 +1,7 @@
+output "cfssl_local_data_volumeid" {
+  value = "disk/by-partlabel/${var.cfssl-partlabel}"
+}
+
 output "etcd_data_volumeids" {
-  value = [for e in var.etcd_members: "disk/by-partlabel/${var.etcd-partlabel}"]
+  value = [for e in var.etcd_members : "disk/by-partlabel/${var.etcd-partlabel}"]
 }
